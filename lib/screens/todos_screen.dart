43d689f428ba003b8a3c2db502191ce6c112395a
Misas10 <manmisa2003@gmail.com>
2022-01-10 21:56:34 +0000

    redesign todos

--- lib/screens/todos_screen.dart
+++ lib/screens/todos_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import '../models/todos_models.dart';
@@ -10,55 +11,90 @@ class TodosScreen extends StatelessWidget {
     // final List<TodoModel> _allTodos = context.watch<Todo>().allTodos;
 
     // Consumer necessary to use Todo provider
-    return Consumer<Todo>(
-      builder: (context, data, child) {
-        return Scaffold(
-          body: ListView.builder(
-            shrinkWrap: true,
-            itemCount: data.allTodos.length,
-            itemBuilder: (context, index) {
-              final todo = data.allTodos[index];
-              return Container(
-                child: _buildTodoItem(todo, data, index),
-                padding: const EdgeInsets.only(right: 18, top: 8),
-              );
-            },
-          ),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Consumer<Todo>(
+        builder: (context, data, child) {
+          return Scaffold(
+            body: ListView.separated(
+              separatorBuilder: (context, _) =>
+                  const Divider(color: Colors.grey),
+              shrinkWrap: true,
+              itemCount: data.allTodos.length,
+              itemBuilder: (context, index) {
+                final todo = data.allTodos[index];
+                return Slidable(
+                  key: Key(todo.text),
+                  endActionPane: ActionPane(
+                    children: [
+                      const SlidableAction(
+                        onPressed: null,
+                        icon: Icons.edit,
+                        label: "Edit",
+                        backgroundColor: Colors.blue,
+                      ),
+                      SlidableAction(
+                        onPressed: (context) {
+                          data.removeTodo(index);
+                        },
+                        icon: Icons.delete,
+                        label: "Delete",
+                        backgroundColor: Colors.red,
+                      )
+                    ],
+                    motion: const StretchMotion(),
+                    dismissible: DismissiblePane(onDismissed: () {
+                      data.removeTodo(index);
+                    }),
+                  ),
+                  child: _buildTodoItem(todo, data, index),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 
-  GestureDetector _buildTodoItem(TodoModel todo, Todo data, int index) {
-    return GestureDetector(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Checkbox(
-            value: todo.isChecked,
-            onChanged: (_) => data.toggleTodo(index),
-          ),
-          Expanded(
-            child: Text(
-              todo.text,
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 16,
-                decoration: todo.isChecked
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
+  Widget _buildTodoItem(TodoModel todo, Todo data, int index) {
+    return Column(
+      children: [
+        GestureDetector(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Checkbox(
+                  value: todo.isChecked,
+                  onChanged: (_) => data.toggleTodo(index),
+                ),
+                Expanded(
+                  child: Text(
+                    todo.text,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      decoration: todo.isChecked
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                ),
+              ],
             ),
+            padding: const EdgeInsets.only(bottom: 10),
           ),
-          Container(width: 8),
-          ElevatedButton(
-            child: const Text("DELETE"),
-            style: ElevatedButton.styleFrom(primary: Colors.red),
-            onPressed: () => data.removeTodo(index),
-          ),
-        ],
-      ),
-      onTap: () => data.toggleTodo(index),
+          onTap: () => data.toggleTodo(index),
+        ),
+
+        // A grey bar between the todos
+        // Container(
+        //   height: 1,
+        //   width: double.infinity,
+        //   color: Colors.grey,
+        // ),
+      ],
     );
   }
 }
